2940a6ed037c12c884d98123ce46400dcc15246b
roberts67HP <robitosins@gmail.com>
2023-05-19 12:54:10 +0300

    Changed skybox and started making the level

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour {
     public float mouseSensitivity = 100f;
     public Transform playerBody;
+    public Camera cam;
+
+    private float startFOV;
+    private float endFOV;
+    private float camIncrement = 0.1f;
 
     private float xRotation = 0f;
 
@@ -12,10 +17,16 @@ public class PlayerCamera : MonoBehaviour {
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        startFOV = cam.fieldOfView;
+        endFOV = cam.fieldOfView + 200f;
     }
 
     // Update is called once per frame
     void Update() {
+        MouseLook();
+        CamBreathe();
+    }
+    private void MouseLook () {
         float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -24,5 +35,14 @@ public class PlayerCamera : MonoBehaviour {
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+    }
+    private void CamBreathe () {
+        // if(cam.fieldOfView > endFOV) {
+        //     Debug.Log(cam.fieldOfView+" - -");
+        //     cam.fieldOfView -= camIncrement;
+        // } else if (cam.fieldOfView < startFOV) {
+        //     Debug.Log(cam.fieldOfView+" - +");
+        //     cam.fieldOfView += camIncrement;
+        // }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,12 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    [Header("References")]
-    public Rigidbody rigidbody;
-    public Transform camHolder;
-    public Camera camera;
-
-    [Header("Configurations")]
+    public Rigidbody rb;
     public float walkSpeed = 5f;
     public float runSpeed = 10f;
     public float jumpSpeed = 6f;
@@ -20,7 +15,6 @@ public class PlayerController : MonoBehaviour {
     float speed;
     Vector3 direction;
     bool isGrounded;
-    bool isJumping;
 
     // Start is called before the first frame update
     void Start() {
@@ -36,8 +30,7 @@ public class PlayerController : MonoBehaviour {
         
     }
     void OnGameLose () {
-        // controller.enabled = false;
-        // rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+
     }
 
     void Update() {
@@ -46,24 +39,21 @@ public class PlayerController : MonoBehaviour {
         direction.z = Input.GetAxis("Vertical");
 
         if(Input.GetButtonDown("Jump") && isGrounded) {
-            isJumping = false;
-            rigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
         }
     }
 
     void FixedUpdate () {
         Vector3 walkVelocity = (transform.right * direction.x + transform.forward * direction.z) * speed;
-        rigidbody.velocity = new Vector3(walkVelocity.x, rigidbody.velocity.y, walkVelocity.z);
+        rb.velocity = new Vector3(walkVelocity.x, rb.velocity.y, walkVelocity.z);
     }
     void OnCollisionEnter (Collision collision) {
         if(collision.collider.CompareTag("Enemy")) {
             GameManager.instance.SetState(GameState.Lose);
         }
-        Debug.Log("on");
         isGrounded = true;
     }
     void OnCollisionExit (Collision collision) {
-        Debug.Log("off");
         isGrounded = false;
     }
 }
